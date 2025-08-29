09c4f098d95eb3fe93a239150045e564c8c5f777
adithyavis <adithyaviswam@gmail.com>
2025-08-30 00:37:43 +0530

    feat: initialize repo

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+export default function App() {
+  return (
+    <GestureHandlerRootView>
+      <View style={styles.container} />
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1c1c1c',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
