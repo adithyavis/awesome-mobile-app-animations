ecf3f64acd698f41d372a04582db961787309aaf
adithyavis <adithyaviswam@gmail.com>
2026-04-16 23:58:05 +0530

    feat: add component

---
+++ src/app/chatgpt-voice-profiles.tsx
@@ -0,0 +1,5 @@
+import ChatGPTVoiceProfilesScreen from '../ChatGPTVoiceProfiles/components/ChatGPTVoiceProfilesScreen';
+
+export default function ChatGPTVoiceProfilesRoute() {
+  return <ChatGPTVoiceProfilesScreen />;
+}
